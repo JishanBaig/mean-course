6a2e32ac38c9d5c3d365c67e82642bd1ef51b8ea
JishanBaig <jishanbaig271291@gmail.com>
2020-09-05 12:12:59 +0530

    Structural directives

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 // lets see if we can out put somew dummy posts.
+// done that
+// time to add post list and post create copmponent.
 
 @Component({
   selector: 'app-post-list',
@@ -8,11 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  posts = [
-    {title: 'First Post', content: 'This is the first post\'s content'},
-    {title: 'Second Post', content: 'This is the second post\'s content'},
-    {title: 'Third Post', content: 'This is the third post\'s content'}
-  ];
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post\'s content'},
+  //   {title: 'Second Post', content: 'This is the second post\'s content'},
+  //   {title: 'Third Post', content: 'This is the third post\'s content'}
+  // ];
+  posts = [];
   // dynamically loop through these posts and output the data.->
   // Angular directives which allows us to manimulate elements in the dom when needed.
 
