2716c71333d1aa8a31bd097ee744d788462f0b55
JishanBaig <jishanbaig271291@gmail.com>
2020-09-05 09:19:14 +0530

    basic comment section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,23 @@
  * modules hwvh definds the building block of the application
  * regitering the component/features with angular.
  * in this case one module
+ * npm install --save @angular/material@8 --save--exact
+ * to install exact agular material
  */
 
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // explicitly register a component
 
 @NgModule({
@@ -27,7 +33,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -5,16 +5,17 @@ import { Component } from '@angular/core';
   // allows to use this component.
   selector: 'app-post-create',
   // angular wil look for this file.
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent{
   newPost = 'NO CONTENT';
   enteredValue = '';
 
   onAddPost() {
-    //alert('post added!');
-    //console.dir(postInput);
-    //this.newPost = postInput.value; // one way to getting user's input.
+    // alert('post added!');
+    // console.dir(postInput);
+    // this.newPost = postInput.value; // one way to getting user's input.
     this.newPost = this.enteredValue;
   }
 }
