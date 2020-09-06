f7013e2669c602447905c20dbd3416b4460b62c6
JishanBaig <jishanbaig271291@gmail.com>
2020-09-06 21:02:19 +0530

    Fethced initial posts

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,29 @@ const app = express();
 // express is a big chain of middleweres
 // use is one middleware function.
 
-app.use( (req,res,next) => {
-  console.log("First Middlewatre");
-  next(); //
-});
+// app.use( (req,res,next) => {
+//   console.log("First Middlewatre");
+//   next(); //
+// });
 
-
-app.use( (req,res,next) => {
-  res.send("Hello from Express");
+app.use( '/api/posts', (req,res,next) => {
+  //res.send("Hello from Express");
+  const posts = [
+    {
+      id: "ff4433445d",
+      title: "First server-side post",
+      content: "this is coming from server"
+    },
+    {
+      id: "ff4433445d",
+      title: "First server-side post",
+      content: "this is coming from server!"
+    }
+  ];
+  res.status(200).json({
+    message: 'Psts fetched successfully!',
+    posts: posts
+  });
   //next(); // request will actually continue its joueney
 });
 
