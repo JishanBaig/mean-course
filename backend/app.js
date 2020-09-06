ad9b728e1c6f010be01b3520c2c68a15b926609f
JishanBaig <jishanbaig271291@gmail.com>
2020-09-06 22:38:56 +0530

    Understanding CORS

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,19 @@ const app = express();
 //   console.log("First Middlewatre");
 //   next(); //
 // });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,POST, PATCH,DELETE, OPTIONS'
+    );
+  next();
+});
+
 
 app.use( '/api/posts', (req,res,next) => {
   //res.send("Hello from Express");
