58130899ef5ab23fed03cc57c002d8f97fc8e8b9
JishanBaig <jishanbaig271291@gmail.com>
2020-09-06 20:50:53 +0530

    improved server.js code

--- server.js
+++ server.js
@@ -1,19 +1,75 @@
-// node.js backend: server.js and backend folder
-// console.log('Hello');
+// old code starts
+// // node.js backend: server.js and backend folder
+// // console.log('Hello');
 
-const http = require('http');
-const app = require('./backend/app');
+// const http = require('http');
+// const app = require('./backend/app');
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-app.set('port', port);
+// app.set('port', port);
+// const server = http.createServer(app);
+// // const server = http.createServer((req, res) => {
+// //   // might be parsing the request.-> Express helps.
+// //   res.end('This is my first response');
+// // });
+
+// // hosting provider's given port number.
+// server.listen(port);
+// old code ends
+
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
+
+
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
 const server = http.createServer(app);
-// const server = http.createServer((req, res) => {
-//   // might be parsing the request.-> Express helps.
-//   res.end('This is my first response');
-// });
-
-// hosting provider's given port number.
+server.on("error", onError);
+server.on("listening", onListening);
 server.listen(port);
 
-
+// constantly quiting and restarting trhe server is annoying so
